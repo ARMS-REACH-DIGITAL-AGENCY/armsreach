27a6f1b9535a0fbdab5302c020f04fa732e0f67d
YAT?STATS <pcdaction@gmail.com>
2026-09-12 16:12:01 -0700

    Load rotating journey hero and live platform preview

--- sites/yatstats/search-template.js
+++ sites/yatstats/search-template.js
@@ -1,6 +1,7 @@
 (() => {
   const ORIGINAL_SEARCH = 'https://cdn.jsdelivr.net/gh/ARMS-REACH-DIGITAL-AGENCY/armsreach@55f327fc90fdbe4b21c214bbebf8cc52570c20b6/sites/yatstats/search-template.js';
   const MODAL_SCRIPT = './subdomain-modal.js';
+  const JOURNEY_HERO_SCRIPT = './hero-journey.js';
 
   function loadScript(src, onload) {
     const script = document.createElement('script');
@@ -10,5 +11,7 @@
     document.head.appendChild(script);
   }
 
-  loadScript(ORIGINAL_SEARCH, () => loadScript(MODAL_SCRIPT));
+  loadScript(ORIGINAL_SEARCH, () => {
+    loadScript(MODAL_SCRIPT, () => loadScript(JOURNEY_HERO_SCRIPT));
+  });
 })();
