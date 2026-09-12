a24b1177f29ad2dec5afdb4d0630bb5863a9eee9
YAT?STATS <pcdaction@gmail.com>
2026-09-12 16:44:45 -0700

    Load guided tour without duplicate search UI

--- sites/yatstats/search-template.js
+++ sites/yatstats/search-template.js
@@ -1,17 +1,9 @@
 (() => {
-  const ORIGINAL_SEARCH = 'https://cdn.jsdelivr.net/gh/ARMS-REACH-DIGITAL-AGENCY/armsreach@55f327fc90fdbe4b21c214bbebf8cc52570c20b6/sites/yatstats/search-template.js';
-  const MODAL_SCRIPT = './subdomain-modal.js';
-  const JOURNEY_HERO_SCRIPT = './hero-journey.js';
+  if (window.__yatHomepageLoader) return;
+  window.__yatHomepageLoader = true;
 
-  function loadScript(src, onload) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    if (onload) script.addEventListener('load', onload, { once: true });
-    document.head.appendChild(script);
-  }
-
-  loadScript(ORIGINAL_SEARCH, () => {
-    loadScript(MODAL_SCRIPT, () => loadScript(JOURNEY_HERO_SCRIPT));
-  });
+  const script = document.createElement('script');
+  script.src = './hero-journey.js';
+  script.async = false;
+  document.head.appendChild(script);
 })();
